1a660d0590330abdd62a0e8bd0ad2b080d70625a
stevenkword <stevenword@gmail.com>
2015-02-10 22:35:02 +0000

    Shortcode support

--- geoip.php
+++ geoip.php
@@ -5,8 +5,8 @@ Version: 1.0.0
 Description: Create a personalized user experienced based on location.
 Author: WP Engine
 Author URI: http://wpengine.com
-Plugin URI: http://www.wpengine.com/wp-geo-ip/
-Text Domain: wp-geo-ip
+Plugin URI: https://wordpress.org/plugins/wpe-geo-ip/
+Text Domain: wpe-geo-ip
 Domain Path: /languages
 */
 
@@ -34,23 +34,46 @@ class GeoIp {
 	// The geographical data loaded from the environment
 	public $geos;
 
+	/* Shortcode */
+	const SHORTCODE_COUNTRY = 'geoip-country';
+	const SHORTCODE_REGION  = 'geoip-region';
+	const SHORTCODE_CITY    = 'geoip-city';
+
+	/**
+	 * [init description]
+	 * @return [type] [description]
+	 */
 	public static function init() {
 		add_action( 'init', array( self::instance(), 'setup' ) );
 	}
 
+	/**
+	 * [instance description]
+	 * @return [type] [description]
+	 */
 	public static function instance() {
 		// create a new object if it doesn't exist.
 		is_null( self::$instance ) && self::$instance = new self;
 		return self::$instance;
 	}
 
+	/**
+	 * [setup description]
+	 * @return [type] [description]
+	 */
 	public function setup() {
 		$this->geos = $this->get_actuals();
+
+		// Initialize
+		add_action( 'init', array( $this, 'action_init_register_shortcodes' ) );
+
 	}
 
 	/**
- 	 * Here we extract the data from headers set by nginx -- lets only send them if they are part of the cache key
- 	 **/
+	 * Here we extract the data from headers set by nginx -- lets only send them if they are part of the cache key
+	 *
+	 * @return [type] [description]
+	 */
 	public function get_actuals() {
 		return array(
 			'countrycode'  => getenv( 'HTTP_GEOIP_COUNTRY_CODE' ),
@@ -68,7 +91,7 @@ class GeoIp {
 	/**
 	 * Examples of easy to use utility functions that we should have for each geo that is part of the cache key
 	 *
-	 * @return mixed
+	 * @return mixed Description
 	 */
 	public function country() {
 		return $this->geos[ 'countrycode' ];
@@ -76,22 +99,85 @@ class GeoIp {
 
 	/**
 	 * Region
-	 * @return mixed
+	 *
+	 * @return mixed Description
 	 */
 	public function region() {
 		return $this->geos[ 'region' ];
 	}
 
 	/**
-	 * @return mixed
+	 *
+	 *
+	 * @return mixed Description
 	 */
 	public function city() {
 		return $this->geos[ 'city' ];
 	}
 
+	/**
+	 * Register the shortcode(s)
+	 *
+	 * @since  1.0.0
+	 * @uses add_shortcode()
+	 * @return null
+	 */
+	public function action_init_register_shortcodes() {
+
+		// Country Shortcode
+		if ( ! shortcode_exists( self::SHORTCODE_COUNTRY ) ) {
+			add_shortcode( self::SHORTCODE_COUNTRY, array( $this, 'do_shortcode_country' ) );
+		}
+
+		// Region Shortcode
+		if ( ! shortcode_exists( self::SHORTCODE_REGION ) ) {
+			add_shortcode( self::SHORTCODE_REGION, array( $this, 'do_shortcode_region' ) );
+		}
+
+		// City Shortcode
+		if ( ! shortcode_exists( self::SHORTCODE_CITY ) ) {
+			add_shortcode( self::SHORTCODE_CITY, array( $this, 'do_shortcode_city' ) );
+		}
+
+	}
+
+	/**
+	 * Output the current country
+	 *
+	 * @since  1.0.0
+	 * @return string $html
+	 */
+	function do_shortcode_country( $atts ) {
+		if( isset( $this->geos[ 'country' ] ) ) {
+			echo $this->geos[ 'country' ];
+		}
+	}
+
+	/**
+	 * Output the current region
+	 *
+	 * @since  1.0.0
+	 * @return string $html
+	 */
+	function do_shortcode_region( $atts ) {
+		if( isset( $this->geos[ 'region' ] ) ) {
+			echo $this->geos[ 'region' ];
+		}
+	}
+
+	/**
+	 * Output the current city
+	 *
+	 * @since  1.0.0
+	 * @return string $html
+	 */
+	function do_shortcode_city( $atts ) {
+		if( isset( $this->geos[ 'city' ] ) ) {
+			echo $this->geos[ 'city' ];
+		}
+	}
+
 }
 
-/**
- * Register to do the stuff
- */
+// Register to do the stuff
 GeoIp::init();
